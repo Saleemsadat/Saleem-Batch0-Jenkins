c4ee1fbacb5b2fe2adabbf4b559b2d10e38058d4
Saleem <saleemsadat1@gmail.com>
2021-08-20 16:24:45 -0400

    commit 3

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Batch 9 is great");
             Thread.sleep(1000);
 
